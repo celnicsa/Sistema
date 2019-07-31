c2fa39c4db3e81e4ba8e7d43ace37dbf56afa103
bryanLch <bryan3108lopez@gmail.com>
2019-07-30 23:32:07 -0600

    Proyecto
    Primero De Bryan Probando

--- AppImelsa/Forms/Nomina/AsistenceForm.vb
+++
@@ -1,3 +0,0 @@
-Public Class AsistenceForm
-
-End Class
--- BD/Oscar/Planilla/Rollback.sql
+++
@@ -1,6 +0,0 @@
-/* ROLLBACK */
-
-   DROP TABLE  MTableBeneficioEscolaridad;
-   ALTER TABLE MTableEMP DROP COLUMN Tipo_Escolaridad;
-   DROP TABLE MTableNominaEmpleado;
-   DROP TABLE MTableNominaDetalle;
